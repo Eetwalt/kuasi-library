4de7b3a8d12794789a5217d7fff8146597fd054a
Eetwalt <eetu@eeturantanen.fi>
2023-04-16 16:12:06 +0300

    added hover state to animationText

--- sandbox.js
+++ sandbox.js
@@ -1,4 +1,5 @@
 const calendarCanvas = document.getElementById("calendar-canvas");
+const animationText = document.getElementById("animationText");
 const calendarButton = new rive.Rive({
   src:
     "https://uploads-ssl.webflow.com/643bea714b119b1c10dce229/643beff8777c73226f2a3e76_calendar-test.riv.txt",
@@ -14,10 +15,11 @@ const calendarButton = new rive.Rive({
     newStates.forEach((state) => {
       //name of the state transitioned to on click and the URL you want
       if (state.indexOf("Linkout") > -1) {
-        window.location.href = "https://eeturantanen.webflow.io/sandbox";
+        window.location.href = "";
         //Add an else if for all states that should have the 'pointer' cursor
       } else if (state.indexOf("on-hover") > -1) {
         calendarCanvas.style.cursor = "pointer";
+        animationText.classList.add("hovered");
       } else if (state.indexOf("Idle") > -1) {
         calendarCanvas.style.cursor = "default";
       }
