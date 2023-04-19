a2a1b35176d030b4f2c9bc77015c952a7a4b31e7
Eetwalt <eetu@eeturantanen.fi>
2023-04-20 00:15:11 +0300

    enabled inde.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-/* const testText = document.querySelector("[kuasi-element=test]");
+const testText = document.querySelector("[kuasi-element=test]");
 
 window.onload = function() {
   console.log("Your script is working!");
   testText.textContent = "Hello home page!";
 };
-*/
