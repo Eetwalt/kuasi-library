edccc1e362afa5ff2790541648c5401a2fe43994
Eetwalt <eetu@eeturantanen.fi>
2023-04-20 00:07:56 +0300

    made side navigation functional

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const testText = document.querySelector("[kuasi-element=test]");
+/* const testText = document.querySelector("[kuasi-element=test]");
 
 window.onload = function() {
   console.log("Your script is working!");
   testText.textContent = "Hello home page!";
 };
+*/
