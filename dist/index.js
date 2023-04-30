6bafe555418adb61af30f376fc1586cea4469173
Eetwalt <eetu@eeturantanen.fi>
2023-04-30 15:43:58 +0300

    test commit

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,6 @@ const homeTestText = document.querySelector("[kuasi-element=test]");
 window.onload = function () {
     console.log("Your script is working!");
     if (homeTestText) {
-        homeTestText.textContent = "Hello home page!";
+        homeTestText.textContent = "Hello home page!!";
     }
 };
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBLE1BQU0sWUFBWSxHQUFHLFFBQVEsQ0FBQyxhQUFhLENBQWlCLHNCQUFzQixDQUFDLENBQUM7QUFFcEYsTUFBTSxDQUFDLE1BQU0sR0FBRztJQUNkLE9BQU8sQ0FBQyxHQUFHLENBQUMseUJBQXlCLENBQUMsQ0FBQztJQUN2QyxJQUFJLFlBQVksRUFBRTtRQUNoQixZQUFZLENBQUMsV0FBVyxHQUFHLGtCQUFrQixDQUFDO0tBQy9DO0FBQ0gsQ0FBQyxDQUFDIn0=
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ const homeTestText = document.querySelector<HTMLDivElement>("[kuasi-element=test
 window.onload = function() {
   console.log("Your script is working!");
   if (homeTestText) {
-    homeTestText.textContent = "Hello home page!";
+    homeTestText.textContent = "Hello home page!!";
   }
 };
